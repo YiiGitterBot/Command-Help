5b9e962609f9a09b572628b991b338d87367fbd8
Alex Bond <alex.bondarevskiy@gmail.com>
2015-11-04 01:37:39 -0800

    1.1 - Multi room support

--- src/main/java/org/YiiCommunity/GitterBot/HelpCommand/Help.java
+++ src/main/java/org/YiiCommunity/GitterBot/HelpCommand/Help.java
@@ -1,6 +1,7 @@
 package org.YiiCommunity.GitterBot.HelpCommand;
 
 import com.amatkivskiy.gitter.rx.sdk.model.response.message.MessageResponse;
+import com.amatkivskiy.gitter.rx.sdk.model.response.room.RoomResponse;
 import org.YiiCommunity.GitterBot.GitterBot;
 import org.YiiCommunity.GitterBot.api.Command;
 import org.YiiCommunity.GitterBot.containers.Gitter;
@@ -17,22 +18,22 @@ public class Help extends Command {
     }
 
     @Override
-    public void onMessage(MessageResponse message) {
+    public void onMessage(RoomResponse room, MessageResponse message) {
         if (message.text.trim().equalsIgnoreCase("@" + GitterBot.getInstance().getConfiguration().getBotUsername())) {
-            showHelp(message.fromUser.username);
+            showHelp(room, message.fromUser.username);
             return;
         }
         for (String item : commands) {
             if (message.text.trim().equalsIgnoreCase("@" + GitterBot.getInstance().getConfiguration().getBotUsername() + " " + item)) {
-                showHelp(message.fromUser.username);
+                showHelp(room, message.fromUser.username);
                 return;
             }
         }
     }
 
-    private void showHelp(String username) {
+    private void showHelp(RoomResponse room, String username) {
         try {
-            Gitter.sendMessage(getConfig().getString("text").replace("{username}", username));
+            Gitter.sendMessage(room, getConfig().getString("text").replace("{username}", username));
         } catch (Exception e) {
             e.printStackTrace();
         }
